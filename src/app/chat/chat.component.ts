5fb680557197576a98eba8fbde4dfe2546ee89a5
mamagic <rrhhww0213@naver.com>
2023-09-14 07:01:20 +0900

    add

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,34 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { WebSocketService } from '../websocket/WebSocketService';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent {
+export class ChatComponent implements OnDestroy{
   messages: { text: string }[] = [];
   newMessage = '';
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient,
+              private webSocketService: WebSocketService,
+              private messageSubscription: Subscription){
   }
 
   sendMessage() {
     if (this.newMessage.trim() !== '') {
-      this.messages.push({ text: this.newMessage });
       this.httpClient.post(
-          'http://localhost:8080/', this.newMessage,
-                                    { headers: { 'Content-Type': 'text/plain;charset=UTF-8' }}
-                                    )
-          .subscribe(response => {
+          'http://localhost:8080/',
+          this.newMessage,
+          { headers:
+            { 'Content-Type': 'text/plain;charset=UTF-8' }
+          })
+          .subscribe((response : any) => {
             console.log(response);
-          },
+
+            if (response && response.text){
+              this.messageSubscription = this.webSocketService.getMessageSubject().subscribe((message: string) => {
+                      this.messages.push({ text: message });
+                  });
+            }
+           },
           (error) => {
-            console.error("send failed",error);
+            console.error("send failed",error.message);
           });
 
       this.newMessage = '';
     }
   }
+
+  ngOnDestroy() {
+      this.messageSubscription.unsubscribe();
+    }
 }
 
---
+++ src/app/websocket/WebSocketService.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Client, over, Message } from 'stompjs';
+import SockJS from 'sockjs-client';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebSocketService{
+  private stompClient: Client = new Client();
+  private messageSubject = new Subject<string>();
+
+  constructor() {
+    this.initializeWebSocketConnection();
+  }
+
+  initializeWebSocketConnection() {
+    const socket = new SockJS('/ws');
+    this.stompClient = over(socket);
+
+    this.stompClient.connect({}, () => {
+      this.stompClient.subscribe('/topic/broadcast-message', (message: Message) => {
+        const receivedMessage = message.body;
+        this.messageSubject.next(receivedMessage);
+      });
+    });
+  }
+
+  getMessageSubject() {
+    return this.messageSubject.asObservable();
+  }
+}
